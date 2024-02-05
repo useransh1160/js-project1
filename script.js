058699f1cc5c11f469d6b4ad28565de08afbc9df
ansh <anshumanmisra690@gmail.com>
2024-02-05 10:49:42 +0530

    hii ,thank you for spending your precious time to see my repository this is a beginner project aiming to shape up my basics of html and css
    Basically in this first git hub push i made an frontend of web-app by using only html and css. Would be developing more high quality projects soon stay tuned

---
+++ script.js
@@ -0,0 +1,18 @@
+console.log("hii");
+let a="wah";
+let b="ladke";
+console.log(a+b);
+const item={
+    name:"led",
+    price:"345"
+    
+
+}
+console.log(item.price);
+let c=prompt("hii please enter your age");
+if(c<13)
+    console.log("you are not a teenager");
+
+if(c>13 && c<=19) console.log("you are starting your teenager ");
+
+else console.log("became elder all the very best for your future");
